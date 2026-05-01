30d12ac1db577e799289d4802ee860c439d9832b
Lisanne Madeleine Godlinski <ligo4999@th-wildau.de>
2026-05-01 14:28:44 +0200

    AboutUs Seite mit HTML Elementen / Infos gemäß "C - About-Us-Inhalte vorbereiten #100"

---
+++ pages/about.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="de-de">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+
+
+    <title>About Us</title>
+</head>
+<body>
+    <h1>About Us</h1>
+    <p>
+        Das hier ist eine hypothetische About-Us Seite für das WebTechnologieprojekt,
+        das wir in diesem Semester bearbeiten müssen.
+    </p>
+
+    <p>
+        Unser Team besteht aus:
+    </p>
+
+    <ul>
+        <li> Lisanne Godlinski </li>
+        <li> Marvin Hicke </li>
+        <li> Linus Meyer </li>
+        <li> Sehyang Na </li>
+        <li> Fatemeh Nezamolmaleki </li>
+    </ul>
+
+    <p>
+        Arbeitsteilung:
+    </p>
+
+    <table>
+        <thead>
+        <tr>
+            <th>Teammitglied</th>
+            <th>Aufgabe</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+            <td>Lisanne Godlinski</td>
+            <td>C - Frontend</td>
+        </tr>
+        <tr>
+            <td>Marvin Hicke</td>
+            <td>A - Backend</td>
+        </tr>
+        <tr>
+            <td>Linus Meyer</td>
+            <td>E - Frontend</td>
+        </tr>
+        <tr>
+            <td>Sehyang Na</td>
+            <td>B - Backend</td>
+        </tr>
+        <tr>
+            <td>Fatemeh Nezamolmaleki</td>
+            <td>D - Frontend</td>
+        </tr>
+        </tbody>
+    </table>
+
+</body>
+</html>
