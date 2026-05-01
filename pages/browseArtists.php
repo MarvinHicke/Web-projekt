d55ccd59a45582f48785f670f2f1e60202cd7ddf
Lisanne Madeleine Godlinski <ligo4999@th-wildau.de>
2026-05-01 14:32:01 +0200

    Browse Seiten mit ersten HTML Platzhalter Elementen/Infos gemäß Kommentar in "C - Browse-Seiten als Struktur vorbereiten #60"

---
+++ pages/browseArtists.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Browse Artists</title>
+    <meta name="Browse all Artists" content="Das ist die Seite die alle enthaltenen Artists anzeigt.">
+</head>
+
+<body>
+<h1>Browse Artists</h1>
+<p>
+    Hier findet man eine Listenübersicht aller Artists. <br>
+    Diese lassen sich nach Name und Vorname sortieren (aufsteigend oder absteigend).
+</p>
+
+<table class="table">
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Vorname</th>
+        <th>Bild</th>
+        <th>Link</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <tr>
+        <td>Nachname 1</td>
+        <td>Vorname 1</td>
+        <td><img src="https://www.ri-fa.de/wp-content/uploads/2022/10/ri-fa-platzhalter-produkt-1500x1500-1.jpg" width="50" alt="Platzhalter Bilder"></td>
+        <td><a href="artist1.php">Link zur zukünftigen Seite</a></td>
+    </tr>
+    <tr>
+        <td>Nachname 2</td>
+        <td>Vorname 2</td>
+        <td><img src="https://www.ri-fa.de/wp-content/uploads/2022/10/ri-fa-platzhalter-produkt-1500x1500-1.jpg" width="50" alt="Platzhalter Bilder"></td>
+        <td><a href="artist2.php">Link zur zukünftigen Seite</a></td>
+    </tr>
+    <tr>
+        <td>Nachname 3</td>
+        <td>Vorname 3</td>
+        <td><img src="https://www.ri-fa.de/wp-content/uploads/2022/10/ri-fa-platzhalter-produkt-1500x1500-1.jpg" width="50" alt="Platzhalter Bilder"></td>
+        <td><a href="artist3.php">Link zur zukünftigen Seite</a></td>
+    </tr>
+    <tr>
+        <td>Nachname 4</td>
+        <td>Vorname 4</td>
+        <td><img src="https://www.ri-fa.de/wp-content/uploads/2022/10/ri-fa-platzhalter-produkt-1500x1500-1.jpg" width="50" alt="Platzhalter Bilder"></td>
+        <td><a href="artist4.php">Link zur zukünftigen Seite</a></td>
+    </tr>
+    <tr>
+        <td>Nachname 5</td>
+        <td>Vorname 5</td>
+        <td><img src="https://www.ri-fa.de/wp-content/uploads/2022/10/ri-fa-platzhalter-produkt-1500x1500-1.jpg" width="50" alt="Platzhalter Bilder"></td>
+        <td><a href="artist5.php">Link zur zukünftigen Seite</a></td>
+    </tr>
+    </tbody>
+</table>
+
+</body>
