b4851906293bd36ea3d7bb8f4ee7b24e93d77eb2
Na, Sehyang <sena8050@th-wildau.de>
2026-05-14 19:29:02 +0200

    Artist-Card-Komponente hinzugefügt

--- components/artist-card.php
+++ components/artist-card.php
@@ -13,7 +13,7 @@
             </p>
 
             <?php
-            $buttonText = 'View Artist';
+            $buttonText = 'Ansehen';
             $buttonHref = 'single-artist.php?id=' . urlencode($artist->getArtistid());
             $buttonVariant = 'primary';
             include __DIR__ . '/button.php';
