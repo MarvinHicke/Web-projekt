1d612c7deb9103cce22baef718c7662959b99a4b
Na, Sehyang <sena8050@th-wildau.de>
2026-05-30 17:38:01 +0200

    localhost(127.0.0.1)

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 
     // DB konfiguration
-    define('DBHOST', 'localhost');
+    define('DBHOST', '127.0.0.1');
     define('DBNAME', 'art');
     define('DBUSER', 'admin');
     define('DBPASS', 'admin');
