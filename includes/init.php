cf8f27091084fb82c97a602f89e764d2882af050
Na, Sehyang <sena8050@th-wildau.de>
2026-05-30 17:39:29 +0200

    edit init.php

--- includes/init.php
+++ includes/init.php
@@ -1,5 +1,8 @@
 <?php
-session_start();
 
-require_once __DIR__ . '/db.php';
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/auth.php';
