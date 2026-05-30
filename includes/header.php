874f547f737384efe25fc99e9413b3fb413add5a
Na, Sehyang <sena8050@th-wildau.de>
2026-05-30 17:43:07 +0200

    fix helper and bootstrap

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/helpers.php';
+require_once __DIR__ . '/bootstrap.php';
 
 $pageTitle = $pageTitle ?? 'Art Gallery';
 $currentSearch = isset($_GET['q']) ? trim((string) $_GET['q']) : '';
@@ -47,7 +47,7 @@ $currentSearch = isset($_GET['q']) ? trim((string) $_GET['q']) : '';
             <li><a href="<?= base_url('pages/advanced-search.php') ?>">Erweiterte Suche</a></li>
 
             <li class="dropdown">
-                <a href="<?= base_url('pages/artworks.php')?>">Durchsuchen</a>
+                <a href="<?= base_url('pages/browse-artworks.php')?>">Durchsuchen</a>
                 <ul class="dropdown-menu">
                     <li><a href="<?= base_url('pages/browse-artworks.php')?>">Kunstwerke</a></li>
                     <li><a href="<?= base_url('pages/browse-artists.php')?>">Künstler</a></li>
@@ -57,7 +57,7 @@ $currentSearch = isset($_GET['q']) ? trim((string) $_GET['q']) : '';
         </ul>
     </nav>
 
-    <form class="global-search" action="<?= base_url('search-results.php')?>" method="get" role="search">
+    <form class="global-search" action="<?= base_url('pages/search-results.php')?>" method="get" role="search">
         <label class="sr-only" for="global-search-input">Kunstwerke oder Künstler suchen</label>
         <input
             id="global-search-input"
