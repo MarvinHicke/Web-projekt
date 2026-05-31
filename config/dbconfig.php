4aeefc6d4b050360991a101ccf3a92c71f5c9629
Hicke, Marvin <mahi1389@th-wildau.de>
2026-05-31 19:39:49 +0200

    Die erste Dokumentation (gar kein Bock mehr) Bug-Fix in Artist

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,8 +1,9 @@
 <?php
 
-    // DB konfiguration
+    /**
+     *  Globale Konfigurationsdaten für DB-Verbindung
+     */
     define('DBHOST', 'localhost');
     define('DBNAME', 'art');
     define('DBUSER', 'admin');
     define('DBPASS', 'admin');
-?>
