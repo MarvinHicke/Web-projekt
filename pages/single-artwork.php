efbc39494783a24e62ce7a8afa2cbb4d6f5e93bb
Hicke, Marvin <mahi1389@th-wildau.de>
2026-06-12 17:14:03 +0200

    Karte UC25

--- pages/single-artwork.php
+++ pages/single-artwork.php
@@ -228,19 +228,68 @@ require_once __DIR__ . '/../includes/header.php';
                                     </h2>
                                     <div id="gallInfo" class="accordion-collapse collapse">
                                         <div class="accordion-body ps-0 small">
+
                                             <?php if ($galleryObj->getGalleryNativeName()): ?>
-                                                <p class="mb-1"><strong>Einheimischer Name:</strong> <?= e($galleryObj->getGalleryNativeName()); ?></p>
+                                                <p class="mb-1"><strong>Einheimischer Name:</strong> <?php echo e($galleryObj->getGalleryNativeName()); ?></p>
                                             <?php endif; ?>
                                             <?php if ($galleryObj->getGalleryCountry()): ?>
-                                                <p class="mb-1"><strong>Land:</strong> <?= e($galleryObj->getGalleryCountry()); ?></p>
+                                                <p class="mb-1"><strong>Land:</strong> <?php echo e($galleryObj->getGalleryCountry()); ?></p>
                                             <?php endif; ?>
                                             <?php if ($galleryObj->getGalleryWebsite()): ?>
                                                 <p class="mb-0"><strong>Website:</strong>
-                                                    <a href="<?= e($galleryObj->getGalleryWebsite()); ?>" target="_blank" rel="noopener">
-                                                        <?= e($galleryObj->getGalleryWebsite()); ?>
+                                                    <a href="<?php echo e($galleryObj->getGalleryWebsite()); ?>" target="_blank">
+                                                        <?php echo e($galleryObj->getGalleryWebsite()); ?>
                                                     </a>
                                                 </p>
                                             <?php endif; ?>
+
+                                            <?php
+                                            // Nur anzeigen wenn Koordinaten vorhanden sind, falls nicht dann wird die Karte hoffentlich ignoriert :p
+                                            if ($galleryObj->getLatitude() != '' && $galleryObj->getLongitude() != '')
+                                            {
+                                             ?>
+                                                <br>
+                                                <p><b>Wo ist das?</b></p>
+
+                                                <div id="map" style="height: 250px; width: 100%;"></div>
+
+                                                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+                                                <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+                                                <script>
+                                                    // PHP Variablen in JS speichern
+                                                    var lat = <?php echo $galleryObj->getLatitude(); ?>;
+                                                    var lon = <?php echo $galleryObj->getLongitude(); ?>;
+
+                                                    var mapSchonDa = false;
+
+                                                    // Notiz an mich - Karte lädt im unsichtbaren Accordion falsch
+                                                    // Maybe StackOverflow: Warten bis das Accordion ganz offen ist...
+                                                    // https://stackoverflow.com/questions/42604005/leaflet-map-not-showing-properly-in-bootstrap-4-collapse <-- Notiz zum Nachlesen, bitte net löschen :>
+                                                    document.getElementById('gallInfo').addEventListener('shown.bs.collapse', function ()
+                                                    {
+
+                                                        if (mapSchonDa == false) {
+                                                            // Karte laden (Zoom-Level 13)
+                                                            var map = L.map('map').setView([lat, lon], 13);
+
+                                                            // Bilder für die Karte :p
+                                                            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                                                                {
+                                                                attribution: '© OpenStreetMap'
+                                                            }).addTo(map);
+
+                                                            // Diesen roten Marker setzen auf der Karte
+                                                            L.marker([lat, lon]).addTo(map)
+                                                                .bindPopup("Galerie ist hier!")
+                                                                .openPopup();
+
+                                                            mapSchonDa = true; // Damit de la Card (die Karte) beim zweiten Aufklappen nicht nochmal lädt...
+                                                        }
+                                                    });
+                                                </script>
+
+                                            <?php } ?>
                                         </div>
                                     </div>
                                 </div>
